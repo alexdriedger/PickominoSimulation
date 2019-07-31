2254e2b93a1f34c437195b119ddadc648db2f096
alexdriedger <aldriedger@gmail.com>
2019-07-31 15:31:49 +0800

    initial commit

---
+++ GameState.py
@@ -0,0 +1,4 @@
+class GameState:
+    def __init__(self, tempthing):
+        self.temp = tempthing
+        self.constant = 5
---
+++ main.py
@@ -0,0 +1,5 @@
+from GameState import GameState
+
+gs = GameState(9)
+print(gs.temp)
+print(gs.constant)
